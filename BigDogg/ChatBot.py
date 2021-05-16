c64012feabff9c481d07eb505599c7ada7b5f5e5
Dhiraj Wishal <wishaldhiraj@gmail.com>
2021-05-16 15:20:24 +0530

    Added support for DMs.
    * BigDogg can now handle DMs are handled by the chat bot.

--- BigDogg/ChatBot.py
+++ BigDogg/ChatBot.py
@@ -1,48 +1,73 @@
 from chatterbot import ChatBot
 from chatterbot.trainers import ListTrainer
 
+chat_bot_dictionary = {}
+chat_bot_trainers = {}
 chat_bot_enables = {}
 previous_bot_input = {}
 
-chat_bot = ChatBot(
-    "BigDogg",
-    storage_adapter='chatterbot.storage.SQLStorageAdapter',
-    logic_adapters=[
-        'chatterbot.logic.BestMatch',
-        'chatterbot.logic.SpecificResponseAdapter',
-        'chatterbot.logic.MathematicalEvaluation',
-        'chatterbot.logic.SpecificResponseAdapter',
-    ],
-    database_uri='sqlite:///database.sqlite3'
-)
-
-chat_bot_trainer = ListTrainer(chat_bot)
+private_chat_bots = {}
+private_chat_bot_trainers = {}
+private_chat_bot_previous_input = {}
 
 
-def train_chat_bot(response):
+def create_new_chat_bot():
+    """
+    Create a new chat bot.
+    :return: The newly created chat bot.
+    """
+    return ChatBot(
+        "BigDogg",
+        storage_adapter='chatterbot.storage.SQLStorageAdapter',
+        logic_adapters=[
+            'chatterbot.logic.BestMatch',
+            'chatterbot.logic.SpecificResponseAdapter',
+            'chatterbot.logic.MathematicalEvaluation',
+            'chatterbot.logic.SpecificResponseAdapter',
+        ],
+        database_uri='sqlite:///database.sqlite3'
+    )
+
+
+def create_new_chat_bot_trainer(chat_bot):
+    """
+    Create a new chat bot trainer.
+    :param chat_bot: The chat bot to be used.
+    :return: The new trainer.
+    """
+
+    return ListTrainer(chat_bot)
+
+
+def train_chat_bot(server_name, response):
     """
     Train the chatterbot using a single response string.
+    :param server_name: The name of the server.
     :param response: The response string.
     :return: None.
     """
-    chat_bot_trainer.train([response])
+
+    chat_bot_trainers[server_name].train([response])
 
 
-def talk_to_chat_bot(input_text):
+def talk_to_chat_bot(server_name, input_text):
     """
     Talk to the chat bot.
+    :param server_name: The name of the server.
     :param input_text: The input text to submit.
     :return: The chat bot's reply.
     """
-    return chat_bot.get_response(input_text)
+
+    return chat_bot_dictionary[server_name].get_response(input_text)
 
 
 def enable_chat_bot(server_name):
     """
     Enable the chat bot for a particular server.
     :param server_name: The name of the server.
-    :return: Nonde
+    :return: None
     """
+
     chat_bot_enables[server_name] = True
     return f"Chat bot enabled for server: {server_name}"
 
@@ -51,7 +76,52 @@ def disable_chat_bot(server_name):
     """
     Disable the chat bot for a particular server.
     :param server_name: The name of the server.
-    :return: Nonde
+    :return: None
     """
+
     chat_bot_enables[server_name] = False
     return f"Chat bot disabled for server: {server_name}"
+
+
+def enable_private_chat_bot(user_name):
+    """
+    Enable chat bot for a private message.
+    :param user_name: The name of the user.
+    :return: None.
+    """
+
+    private_chat_bots[user_name] = create_new_chat_bot()
+    private_chat_bot_trainers[user_name] = create_new_chat_bot_trainer(private_chat_bots[user_name])
+
+
+def disable_private_chat_bot(user_name):
+    """
+    Disable the private chat bot.
+    :param user_name: Name of the user.
+    :return: None.
+    """
+
+    private_chat_bots.pop(user_name)
+    private_chat_bot_trainers.pop(user_name)
+
+
+def private_chat_bot_get_response(user_name, user_data):
+    """
+    Get a response from the bot.
+    :param user_name: The name of the user.
+    :param user_data: The user inputs.
+    :return: The bot's response.
+    """
+
+    return private_chat_bots[user_name].get_response(user_data)
+
+
+def train_private_chat_bot(user_name, training_string):
+    """
+    Train a private chat bot.
+    :param user_name: The name of the user.
+    :param training_string: The string to be used to train.
+    :return: None
+    """
+
+    private_chat_bot_trainers[user_name].train([training_string])
--- BigDogg/Main.py
+++ BigDogg/Main.py
@@ -16,6 +16,22 @@ def check_if_author_is_stonks(author):
     return author.name == "Stonks" and author.discriminator == "4423"
 
 
+def handle_private_message(message):
+    """
+    Handle private messages (DMs).
+    :param message: The user sent message.
+    :return: The statement to be sent to the user.
+    """
+    user_name = message.author.name + "#" + message.author.discriminator
+
+    enable_private_chat_bot(user_name)
+    train_private_chat_bot(user_name, message.content)
+    bot_response = str(private_chat_bot_get_response(user_name, message.content))
+    train_private_chat_bot(user_name, bot_response)
+
+    return bot_response
+
+
 @client.event
 async def on_ready():
     print('We have logged in as {0.user}'.format(client))
@@ -27,28 +43,35 @@ async def on_message(message):
         return
 
     content = message.content.lower()
-    if content.startswith(begin_command) or content.endswith(begin_command):
+    if message.channel.type == discord.ChannelType.private:
+        await message.channel.send(handle_private_message(message))
+
+    elif content.startswith(begin_command) or content.endswith(begin_command):
         if "print decision tree" in content:
             await message.channel.send(decision_tree.print_all_nodes())
         else:
             await message.channel.send(decision_tree.get_callback(content)(message))
 
     else:
+        server_name = message.guild.name
+
         try:
-            if chat_bot_enables[message.guild.name]:
-                train_chat_bot(message.content)
-                new_response = str(talk_to_chat_bot(message.content))
-                train_chat_bot(new_response)
-                previous_bot_input[message.guild.name] = new_response
+            if chat_bot_enables[server_name]:
+                train_chat_bot(server_name, message.content)
+                new_response = str(talk_to_chat_bot(server_name, message.content))
+                train_chat_bot(server_name, new_response)
+                previous_bot_input[server_name] = new_response
 
                 await message.channel.send(new_response)
 
             elif (len(message.attachments) == 0) and ("http" not in message.content):
-                train_chat_bot(message.content)
+                train_chat_bot(server_name, message.content)
 
         except KeyError:
-            chat_bot_enables[message.guild.name] = False
-            train_chat_bot(message.content)
+            chat_bot_enables[server_name] = False
+            chat_bot_dictionary[server_name] = create_new_chat_bot()
+            chat_bot_trainers[server_name] = create_new_chat_bot_trainer(chat_bot_dictionary[server_name])
+            train_chat_bot(server_name, message.content)
 
 
 client.run(os.environ["DISCORD_BOT_TOKEN"])
